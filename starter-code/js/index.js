d9c33412d0d63440f72a7a95e408ee03b5522c82
unknown <laetitiaroche53@gmail.com>
2018-10-06 12:02:49 +0200

    Total price

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,17 +1,29 @@
-function deleteItem(e){
-
+function deleteItem(){
+  return 0;
 }
 
 function getPriceByProduct(itemNode){
-
+  var itemTotalPrice = itemNode.getElementsByClassName('total-price');
+  var itemPrice = itemNode.getElementsByClassName('item-price');
+  var itemQuantity = itemNode.getElementsByClassName('item-quantity');
+  return itemTotalPrice[0].innerHTML=itemPrice[0].innerHTML * itemQuantity[0].value;
 }
+
 
 function updatePriceByProduct(productPrice, index){
 
 }
 
 function getTotalPrice() {
+  var wrapper = document.getElementsByClassName('wrapper');
+  var totalPrice = document.getElementById('total-price');
+  var sum=0;
+  for(i=0;i<wrapper.length;i++){
+    getPriceByProduct(wrapper[i]);
+    sum += getPriceByProduct(wrapper[i]);
+  }
 
+  totalPrice.innerHTML='$'+sum;
 }
 
 function createQuantityInput(){
@@ -21,6 +33,8 @@ function createQuantityInput(){
 function createDeleteButton(){
 
 }
+
+console.log(deleteItem);
 
 function createQuantityNode(){
 
